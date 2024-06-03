00594fe9acc4a88b1543ce3e05ed19cfd3ec5ba0
Ben <victor.ghiggo.d@uni.pe>
2024-06-03 00:24:39 -0500

    cambios 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React, { useContext, useEffect } from 'react';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import "./App.scss";
+import MoonIcon from "./assets/icons/moon.svg";
+import SunIcon from "./assets/icons/sun.svg";
+import { auth } from "./config/firebase"; // Actualizar la ruta
+import { DARK_THEME, LIGHT_THEME } from "./constants/themeConstants";
+import { ThemeContext } from "./context/ThemeContext";
+import BaseLayout from "./layout/BaseLayout";
+import Chatbot from "./screens/chatbot/Chatbot";
+import CoursesScreen from "./screens/courses/CoursesScreen";
+import Dashboard from "./screens/dashboard/DashboardScreen";
+import PageNotFound from "./screens/error/PageNotFound";
+import Login from "./screens/login/Login";
+import ObjectDetection from "./screens/objectDetection/ObjectDetection";
+import TutoringScreen from "./screens/tutoring/TutoringScreen"; // Asegúrate de importar TutoringScreen
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (theme === DARK_THEME) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        if (location.pathname !== "/login") {
+          navigate("/login");
+        }
+      // } else if (!user.email.endsWith("@uni.pe")) {
+      //   auth.signOut();
+      //   navigate("/login");
+      }
+    }
+  }, [user, loading, navigate, location.pathname]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {!user ? (
+          <Route path="/login" element={<Login />} />
+        ) : (
+          <Route element={<BaseLayout />}>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/courses" element={<CoursesScreen />} />
+            <Route path="/chatbot" element={<Chatbot />} />
+            <Route path="/deteccion" element={<ObjectDetection />} />
+            <Route path="/tutoring" element={<TutoringScreen />} /> {/* Añadir la ruta de tutoría */}
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        )}
+      </Routes>
+
+      <button
+        type="button"
+        className="theme-toggle-btn"
+        onClick={toggleTheme}
+      >
+        <img
+          className="theme-icon"
+          src={theme === LIGHT_THEME ? SunIcon : MoonIcon}
+        />
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import { SidebarProvider } from "./context/SidebarContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
+    <SidebarProvider>
+      <Router>
+        <App />
+      </Router>
+    </SidebarProvider>
+  </ThemeProvider>
+);
